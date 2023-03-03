56aba2ab1b2ac0062512d695998589f9e4581597
shiva-verma <jztshiva@gmail.com>
2023-03-03 14:30:15 +0530

    intial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {ApolloClient, ApolloProvider} from '@apollo/client'
+import { HttpLink } from '@apollo/client';
+import {InMemoryCache} from '@apollo/client';
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+cache,
+link:new HttpLink({
+   uri:'https://delicate-wombat-59.hasura.app/v1/graphql',
+headers:{
+   'x-hasura-admin-secret':'bYIM15TxCi5rZ1agc7JA9CpNxmtkc9M1OTGAXJCtPiFUc2I2W0N7IHD5qF7cYrIw'
+}
+     }),
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+          <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
